5899d9cd47585b4cba5bc3b01865293e996fa2cf
Roger Cheng <roger.random@outlook.com>
2025-02-03 20:00:17 -0800

    Wing knob

--- adjustable_arm.py
+++ adjustable_arm.py
@@ -163,7 +163,7 @@ mid_joint = (
 
 wedge_fastener_diameter = 6.5
 wedge_angle = 25
-wedge_range_vertical = wedge_range_horizontal / math.tan(math.radians(wedge_angle))
+wedge_range_vertical = wedge_range_horizontal * math.tan(math.radians(wedge_angle))
 
 wedge_block_upper_slice = (
     cq.Workplane("XY")
@@ -237,7 +237,7 @@ wedge_block_upper_full_height = (
     cq.Workplane("XY")
     .transformed(offset=cq.Vector(0, arm_length, 0))
     .circle(wedge_fastener_diameter/2)
-    .extrude(wedge_diameter/2, both=True)
+    .extrude(wedge_diameter, both=True)
     )
 
 wedge_hex_z = (
@@ -256,17 +256,23 @@ wedge_block_hex_bolt_head = (
     )
 
 mid_joint_trim = (
-    # Volume above the top of hex head.
+    # Volume used for trimming objects in order to fit in mid joint
     cq.Workplane("XY")
-    .transformed(offset=cq.Vector(0, arm_length, wedge_hex_z + fastener_hex_thickness))
+    .transformed(offset=cq.Vector(0, arm_length, 0))
     .circle(ball_surround_outer_radius)
     .extrude(ball_surround_outer_radius * 2)
     )
 
 wedge_block_hex_bolt = (
     wedge_block_upper_full_height
-    - mid_joint_trim
+    - mid_joint_trim.translate((0, 0, wedge_hex_z + fastener_hex_thickness))
     - wedge_block_hex_bolt_head
+    )
+
+wedge_block_z = wedge_range_vertical + wedge_diameter * math.tan(math.radians(wedge_angle)) / 2
+wedge_block_no_hex = (
+    wedge_block_upper_full_height
+    - mid_joint_trim.translate((0, 0, wedge_block_z))
     )
 
 if reposition_for_printing:
@@ -275,8 +281,14 @@ if reposition_for_printing:
         .translate((ball_surround_outer_radius*2,-arm_length,0))
         .rotate((0, 0, 0), (1, 0, 0), wedge_angle),
         options={"color":"red", "alpha":0.5})
+    show_object(
+        wedge_block_no_hex
+        .translate((-ball_surround_outer_radius*2,-arm_length,0))
+        .rotate((0, 0, 0), (1, 0, 0), wedge_angle),
+        options={"color":"red", "alpha":0.5})
 else:
-    show_object(wedge_block_hex_bolt, options={"color":"red", "alpha":0.5})
+    #show_object(wedge_block_hex_bolt, options={"color":"red", "alpha":0.5})
+    show_object(wedge_block_no_hex, options={"color":"red", "alpha":0.5})
 
 # Assemble half of the arm. Print this twice for the three-jointed mechanism.
 arm = (
@@ -285,43 +297,57 @@ arm = (
     + mid_joint
     - actuating_rod_channel
     - mid_joint_clearance
-    - mid_joint_trim.translate((0,0,-wedge_range_vertical))
+    - mid_joint_trim.translate((0, 0, wedge_block_z - wedge_range_vertical * 2))
     + actuating_rod
     - arm_end_ball_cavity
     )
 
 
-"""
-knob_height = 20
-knob_bottom = 0.6
+knob_base_taper_height = wedge_block_z + ball_surround_outer_radius - wedge_diameter/2
+knob_base_vertical_height = ball_surround_outer_radius + wedge_range_vertical
+knob_base_radius = ball_surround_outer_radius - minimum_gap
+knob_wing_radius = 35
+knob_wing_thickness = 30
+knob_wing_height = ball_surround_outer_radius + (knob_wing_radius - ball_surround_outer_radius)
+knob_bottom = 1.8
 
 knob_hex_head = (
     cq.Workplane("XY")
-    .transformed(offset=cq.Vector(
-        0,
-        arm_length,
-        mid_joint_ramp_height/2 + mid_joint_ramp_vertical_offset + knob_bottom))
-    .polygon(6, 11, circumscribed = True)
-    .extrude(knob_height)
+    .transformed(offset=cq.Vector(0, arm_length, wedge_block_z + knob_bottom))
+    .polygon(6, fastener_hex_width, circumscribed = True)
+    .extrude(knob_wing_height)
     )
 
 knob = (
-    cq.Workplane("XY")
-    .transformed(offset=cq.Vector(
-        0,
-        arm_length,
-        mid_joint_ramp_height/2 + mid_joint_ramp_vertical_offset))
-    .polygon(12,
-        mid_joint_diameter - mid_joint_shell_perimeter*2,
-        circumscribed = False)
-    .circle(mid_joint_fastener_diameter/2)
-    .extrude(knob_height)
-    .faces(">Z")
-    .fillet(2)
+    cq.Workplane("XZ")
+    .transformed(offset=cq.Vector(0, 0, -arm_length))
+    # Base will stay intact
+    .lineTo(0,                wedge_block_z, forConstruction = True)
+    .lineTo(wedge_diameter/2, wedge_block_z)
+    .lineTo(knob_base_radius, knob_base_taper_height)
+    .lineTo(knob_base_radius, knob_base_vertical_height)
+    # Top will be trimmed to form wings
+    .lineTo(knob_wing_radius, knob_wing_height)
+    .lineTo(0,                knob_wing_height)
+    .close()
+    .revolve(360, (0, 0, 0), (0, 1, 0))
+    .edges(">Z").fillet(5)
+    .faces("<Z").workplane()
+    .circle(fastener_diameter/2)
+    .cutThruAll()
     ) - knob_hex_head
 
+knob_removal = (
+    cq.Workplane("XZ")
+    .lineTo(knob_wing_thickness/2, ball_surround_outer_radius*2, forConstruction = True)
+    .rect(knob_wing_radius, knob_wing_height, centered = False)
+    .extrude(- arm_length - knob_wing_radius*2)
+    .edges("|Y")
+    .fillet(5)
+    )
+
+knob = knob - knob_removal - knob_removal.mirror("YZ")
 show_object(knob, options={"color":"green","alpha":0.5})
-"""
 
 combined = end_ball_assembly + arm
 
@@ -335,5 +361,3 @@ if reposition_for_printing:
     show_object((combined - chop).translate((0,0,-cutoff_z)), options={"color":"blue", "alpha":0.5})
 else:
     show_object(combined - chop, options={"color":"blue", "alpha":0.5})
-
-# 2345678901234567890123456789012345678901234567890123456789012345678901234567890
